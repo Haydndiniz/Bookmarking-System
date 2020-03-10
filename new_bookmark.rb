0523fccde63af11e8f8a764198dce1440c912577
Nada Abouward <naeabouward1@sheffield.ac.uk>
2020-03-10 17:35:24 +0000

    Update new_bookmark.rb version-2

--- new_bookmark.rb
+++ new_bookmark.rb
@@ -8,7 +8,12 @@ get '/' do
     erb :new_bookmark
 end
 
+@db = SQLite3::Database.new './Database.db'
+
 post '/new_bookmark' do
+    insertInto = "INSERT INTO Users(name, link, description) VALUES" (
+        params[:name_of_bookmark], params[:link], params[:description]
+    );
     puts params
     redirect '/'
 end
