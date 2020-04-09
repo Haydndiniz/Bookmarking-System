2d4dd2057250f42ef1506b0a6b9423091144a9f2
Igor Kolasa <ikkolasa1@sheffield.ac.uk>
2020-04-09 14:30:13 +0000

    updated bookmarks model with function new

--- models/bookmarks.rb
+++ models/bookmarks.rb
@@ -1,1 +1,15 @@
-TODO: make bookmark models
+module Bookmark
+    
+    def Bookmark.new(bookmark_id, bookmark_name, link, description, creator, last_updated)
+        result = false
+        query = "INSERT INTO bookmarks (bookmark_id, bookmark_name, link, description, creator, last_updated) VALUES(?, ?, ?, ?, ?, ?);"
+        begin
+            $db.execute query, bookmark_id, bookmark_name, link, description, creator, last_updated
+            result = true
+        rescue SQLite3::ConstraintException
+            puts "Insertion Failed"
+        end
+        return result
+    end
+        
+end
