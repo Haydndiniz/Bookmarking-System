0fb021c446935ea0530afff0b4fd2a98bd906bb8
Haydn Diniz <hadiniz1@sheffield.ac.uk>
2020-04-10 16:25:13 +0000

    fixed new_bookmark.rb

--- app/new_bookmark.rb
+++ app/new_bookmark.rb
@@ -1,20 +1,12 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'sinatra/cookies'
-set :bind, '0.0.0.0'
-enable :sessions
 
 get '/new_bookmark' do
-    redirect '/new_bookmark' if session[:logged_in]
     erb :new_bookmark
 end
 
-@db = SQLite3::Database.new './Database.db'
-
 post '/new_bookmark' do
-    insertInto = "INSERT INTO Users(name, link, description) VALUES" (
-        params[:name_of_bookmark], params[:link], params[:description]
-    );
+    insert = "INSERT INTO bookmarks (bookmark_name, link, description) VALUES (?,?,?)"
+    $db.execute insert, params[:name_of_bookmark], params[:link], params[:description]
+    
     puts params
     redirect '/'
 end
