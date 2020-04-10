71f353190c43a1f6ce5110cbe855191d0eed4c6f
Nada Abouward <naeabouward1@sheffield.ac.uk>
2020-04-10 17:27:43 +0000

    fixed the newbookmark files and css indentation

--- app/new_bookmark.rb
+++ app/new_bookmark.rb
@@ -5,7 +5,7 @@ end
 
 post '/new_bookmark' do
     insert = "INSERT INTO bookmarks (bookmark_name, link, description) VALUES (?,?,?)"
-    $db.execute insert, params[:name_of_bookmark], params[:link], params[:description]
+    $db.execute insert, params[:bookmark_name], params[:link], params[:description]
     
     puts params
     redirect '/'
