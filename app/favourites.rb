c7c80257666c94fe1c17f8f39236e0fe0b754f2d
Igor Kolasa <ikkolasa1@sheffield.ac.uk>
2020-05-22 12:41:11 +0000

    favourites button added to add bookmarks to favourites list

--- app/favourites.rb
+++ app/favourites.rb
@@ -1,13 +1,7 @@
-get'/favourites' do
-   
-   if !session[:logged_in]  
-      flash[:warning] = "Please login to continue"
-      redirect '/login'
-   else   
+before do
        @bookmark_ids_in_favourites = Array.new
        @favourite_list_array = Array.new
-       @last_visit = Array.new
-
+     
        query = "SELECT bookmark_id FROM favourites WHERE user_id = ?"
        @bookmark_ids_in_favourites = $db.execute query, session[:user_id]
 
@@ -17,6 +11,29 @@ get'/favourites' do
        end
    
        @favourite_list = @favourite_list_array
-       erb:favourite 
+       
+end
+
+
+
+get'/favourites' do
+   
+   if !session[:logged_in]  
+      flash[:warning] = "Please login to continue"
+      redirect '/login'
+   else   
+       erb :favourite 
     end 
+    
+end
+
+post '/add_to_favourites' do
+    @user_id = session[:user_id]
+    @favourite_bookmark_id = params[:bookmark_id].to_i
+ 
+    if !@favourite_list_array.include? (Bookmark.find_by_id(@favourite_bookmark_id))  
+        Favourite.new(@user_id, @favourite_bookmark_id)
+    end 
+    
+    redirect '/'
 end
--- models/favourite_model.rb
+++ models/favourite_model.rb
@@ -2,7 +2,7 @@ module Favourite
 
     def Favourite.new(user_id, bookmark_id)
         result = false
-        insert = "INSERT INTO favourites (user_id, bookmark_id) VALUES (?,?,?)"
+        insert = "INSERT INTO favourites (user_id, bookmark_id) VALUES (?,?)"
         begin
             $db.execute insert, user_id, bookmark_id
             result = true
